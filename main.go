a9179056515ce11a866683f5d2181291e793f887
Silvano Neto <silvanodev@gmail.com>
2021-04-24 16:50:48 -0300

    add section 1.3 code examples and solve exercise 1.4

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/silvanoneto/go-learning/chapter01"
 	"github.com/silvanoneto/go-learning/util"
 )
 
@@ -9,23 +8,19 @@ func main() {
 	results := []util.Result{
 		{
 			FunctionsList: []func(){
-				chapter01.HelloWorld,
-				chapter01.Echo1,
-				chapter01.Echo2,
-				chapter01.Echo3,
-				chapter01.Exercise01,
-				chapter01.Exercise02,
-				chapter01.Exercise03,
+				// chapter01.HelloWorld,
+				// chapter01.Echo1,
+				// chapter01.Echo2,
+				// chapter01.Echo3,
+				// chapter01.Exercise01,
+				// chapter01.Exercise02,
+				// chapter01.Exercise03,
+				// chapter01.Dup1,
+				// chapter01.Dup2,
+				// chapter01.Dup3,
+				// chapter01.Exercise04,
 			},
 			Name: "Chapter 01",
-		},
-		{
-			FunctionsList: []func(){},
-			Name:          "Chapter 02",
-		},
-		{
-			FunctionsList: nil,
-			Name:          "Chapter 03",
 		},
 	}
 
